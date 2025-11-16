8f9ab63c6039798c6d2369019ba509674774dc1b
danyalfun <danyal.fun@gmail.com>
2025-11-16 09:59:00 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace ConsoleApp1
 {
@@ -85,12 +87,164 @@ namespace ConsoleApp1
                     price = 12 ;
             }
         }
+        static void Task4() 
+        {
+            int age;
+            Console.WriteLine("enter your age");
+            age = int.Parse(Console.ReadLine());
+            double prie = 0;
+            if (age <= 18 && age > 0)
+            {
+                bool student;
+                Console.WriteLine("do you a student");
+                student = bool.Parse(Console.ReadLine());
+                prie = 120;
+                if (student == true)
+                    prie *= 0.8;
+                
+            }
+            else if (age < 60 && age > 19)
+            {
+                prie = 180;
+                bool vip;
+                Console.WriteLine("do you vip");
+                vip = bool.Parse(Console.ReadLine());
+                if (vip == true)
+                    prie *= 1.3;
+
+            }
+            else
+            {
+                prie = 150;
+                bool gimle;
+                Console.WriteLine("do you a gimle from zhl");
+                gimle = bool.Parse(Console.ReadLine());
+                if (gimle == true)
+                    prie -= prie * 0.15;
+
+            }
+            Console.WriteLine("you need to pay: " +  prie);    
+        }
+        static void Task5()
+        {
+            string room;
+            double preis = 0;
+            bool endweek;
+            Console.WriteLine("you want swit or reg");
+            room = Console.ReadLine();
+            Console.WriteLine("do you came at weed end");
+            endweek = bool.Parse(Console.ReadLine());
+            bool friend;
+            Console.WriteLine("do you friend");
+            friend = bool.Parse(Console.ReadLine());
+            if (room == "swit")
+            {
+                preis = 400;
+                if (endweek == true)
+                    preis += preis * 0.1;
+                if (friend == true)
+                    preis -= preis * 0.15;
+            }
+            else
+            {
+                preis = 700;
+                if (endweek == true)
+                    preis += preis * 0.1;
+                if (friend == true)
+                    preis -= preis * 0.15;
+            }
+            Console.WriteLine("you need to pay: " + preis);
+
+        }
+        static void Task6()
+        {
+            int age;
+            Console.WriteLine("enter your age");
+            age = int.Parse(Console.ReadLine());
+            double prie = 0;
+            if (age <= 12 && age >= 0)
+            {
+                bool shabt;
+                Console.WriteLine("do you came at shabt");
+                shabt = bool.Parse(Console.ReadLine());
+                prie = 50;
+                if (shabt == true)
+                    prie += 10;
+
+            }
+            else if (age < 18 && age >= 13)
+            {
+                prie = 70;
+                bool member;
+                Console.WriteLine("do you member");
+                member = bool.Parse(Console.ReadLine());
+                if (member == true)
+                    prie *= 0.8;
+
+            }
+            else
+            {
+                prie = 100;
+                bool vip;
+                Console.WriteLine("do you a vip");
+                vip = bool.Parse(Console.ReadLine());
+                if (vip == true)
+                    prie += prie * 0.3;
+
+            }
+            Console.WriteLine("you need to pay: " + prie);
+        }
+       
+        static void Task7()
+        {
+            int age;
+            Console.WriteLine("enter your age");
+            age = int.Parse(Console.ReadLine());
+            double prie = 0;
+            if (age <= 24 && age >= 0)
+            {
+                bool spurt;
+                Console.WriteLine("do you spurt car");
+                spurt = bool.Parse(Console.ReadLine());
+                prie = 600;
+                if (spurt == true)
+                    prie += prie * 0.25;
+
+            }
+            else if (age < 25 && age >= 50)
+            {
+                prie = 450;
+                bool acseddnt;
+                Console.WriteLine("do you was in a acseddnt last year");
+                acseddnt = bool.Parse(Console.ReadLine());
+                if (acseddnt == true)
+                    prie += prie * 0.2;
+
+            }
+            else
+            {
+                prie = 400;
+                bool regular;
+                Console.WriteLine("do you a regular car");
+                regular = bool.Parse(Console.ReadLine());
+                if (regular == true)
+                    prie -= prie * 0.1;
+
+            }
+            Console.WriteLine("you need to pay: " + prie);
+        }
+      
+
+
         static void Main(string[] args)
         {
             //Task1();
             //Task2();
-            Task3();
-
+            //Task3();
+            //Task4();
+            //Task5();
+            //Task6();
+            //Task7();
         }
     }
 }
